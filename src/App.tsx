74582088b38f22585e9b154f08c86050074b765d
Nikaumjs <nikkolas21082003@gmail.com>
2024-10-16 10:20:00 -0300

    refactor: simplify App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { CodeEditor } from "./components/Editor/CodeEditor";
 
 export function App() {
   return (
-    <main className="min-h-screen bg-zinc-800 text-zinc-100">
-      <CodeEditor />
-    </main>
+    <CodeEditor />
   );
 }
