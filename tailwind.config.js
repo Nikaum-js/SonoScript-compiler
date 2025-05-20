9e5d1f551c56753c6aa9f1d5807d484a484d6832
Nikaumjs <nikkolas21082003@gmail.com>
2025-05-20 16:08:34 -0300

    fix: fixing build error :hammer:

--- tailwind.config.js
+++ tailwind.config.js
@@ -71,5 +71,9 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+  ],
 } 
